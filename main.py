803a506c98d02e3eb1bb6a0b08b2a9b12cd7d42a
MuhammadAhmedev <MuhammadAhmed@unialsolutions.com>
2024-04-22 11:47:55 +0500

    change range

--- main.py
+++ main.py
@@ -37,6 +37,6 @@ def get_posts():
 @app.post("/posts")
 def createPost(post:Post):
     post_dict = post.dict()
-    post_dict["id"] = randrange(0 , 100000000)
+    post_dict["id"] = randrange(0 , 10000000)
     my_posts.append(post_dict)
     return {"data":post_dict}  
